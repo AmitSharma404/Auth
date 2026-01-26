95f9f979c3b3198f879c1c44d36abdfa8a95d5c7
AmitSharma <Sandy31524@gmail.com>
2026-01-26 10:02:45 +0530

    Animation

--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -1,12 +1,34 @@
+import { useEffect, useState } from "react"
 import ShortUrl from "../Components/ShortUrl"
 
 export const Home = () => {
+
+    const [text,setText] = useState("");
+    const fullText = "Url Shortner"
+
+    useEffect(() => {
+      let index = 0;
+      const interval = setInterval(() => {
+          setText(fullText.substring(0,index))
+          index++;
+
+          if(index > fullText.length){
+            clearInterval(interval);
+          }
+      }, 200);
+      return () =>  clearInterval(interval)
+    },[])
+    
     return (
-        <div className="min-h-screen w-full flex justify-center items-center flex-col space-y-6 bg-linear-to-lt from-green-500 to-lime-400 bg-stone-200">
-          <div className="neumorphism px-4 py-1 rounded-[40px] squircle">
-            <h1 className="font-extrabold text-4xl sm:text-6xl text-transparent bg-clip-text bg-linear-to-r/decreasing from-indigo-500 to-green-500">Url Shortener</h1>
+        <div className="flex justify-center items-center flex-col space-y-6 bg-linear-to-lt from-green-500 to-lime-400 bg-stone-200">
+          <div className="flex items-center flex-col gap-4 border border-dashed border-neutral-600/60 min-h-screen px-20 justify-center">
+            <div className="neumorphism px-4 py-1 rounded-[40px] squircle">
+            <h1 className="font-extrabold text-4xl sm:text-6xl text-transparent bg-clip-text bg-linear-to-r/decreasing from-indigo-500 to-green-500">{text}</h1>
           </div>
-            <ShortUrl/>
+            <div>
+              <ShortUrl/>
+            </div>
+          </div>
          </div> 
     )
 }
