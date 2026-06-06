38c551b8e03c107fc57c6274bfdac2c65eff77f1
AmitSharma <Sandy31524@gmail.com>
2026-06-06 10:15:31 +0530

    redirected website UI changed

--- Client/src/Components/RedirectTorealUrl.jsx
+++ Client/src/Components/RedirectTorealUrl.jsx
@@ -8,15 +8,18 @@ export const RedirectTorealUrl = () => {
     console.log(shortCode);
     const redirect = async () => {
         const response = await api.get(`/${shortCode}`);
-        console.log(response.data.longUrl);
-        window.location.href = response.data.longUrl;
+        // console.log(response.data.longUrl);
+        // window.location.href = response.data.longUrl;
     }
     useEffect(() => {
         redirect();
     })
     return (
         <div className="flex min-h-100 w-full items-center justify-center">
-            <h1 className="bg-teal-300 text-3xl text-white px-6 py-2 rounded-full font-bold">Redirecting...</h1>
+        <div className="rounded-2xl border border-teal-400 bg-teal-200 p-20 shadow-md">
+          <h1 className="bg-linear-to-b from-teal-500 to-teal-800 text-3xl text-white px-6 py-2 rounded-full font-bold">Redirecting...</h1>
+          
+        </div>
         </div>
     )
 };
